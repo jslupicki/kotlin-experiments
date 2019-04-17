01ac87c81da91272fe1017c7d9ee4bccf8c0a719
jslupicki <jan@slupicki.com>
2019-04-17 21:31:25 +0200

    Remove unnecessary argument of main()

--- src/main/kotlin/com/slupicki/Functors.kt
+++ src/main/kotlin/com/slupicki/Functors.kt
@@ -4,7 +4,7 @@ inline infix fun <T1, T2, T3> ((T1) -> T2).map(crossinline g: (T2) -> T3): (T1) 
     return { x -> g(this(x)) }
 }
 
-fun main(args: Array<String>) {
+fun main() {
 
     val fString = { a: Int -> "Step1($a)" } map { a: String -> "Step2{$a}" } map { a: String -> "Step3<$a>" }
 
